611b4589788e64db750fb34c6fefafe3e97f798e
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-01-06 13:42:16 -0600

    completed user authentication assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/signup'
+      redirect_to '/'
+    end
   end
 
   private
